9afd8221a0db0787d657e36aea5ec2f86a634740
calculusky <calculusky@gmail.com>
2022-02-28 10:50:24 +0100

    fixing build issues

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    moduleDirectories: ["node_modules"],
+    collectCoverageFrom: [
+        "**/*.{ts,tsx,js,jsx}"
+    ],
+    reporters: ["default"],
+
+    coverageThreshold: {
+        "global": {
+            "branches": 100,
+            "functions": 100,
+            "lines": 100,
+            "statements": 100
+        }
+    },
+
+    testEnvironment: "node",
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)(spec|test).+(ts|tsx|js)"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    roots: [
+        "./"
+    ],
+    testURL: "http://localhost/",
+}
